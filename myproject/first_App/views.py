b4bcf829d6870e0ea716b0d156f5594d3cec47d6
Girish-Gowda <109037149+Girish-Gowda-H@users.noreply.github.com>
2024-04-16 10:14:09 +0530

    Data is passed to the template

--- myproject/first_App/views.py
+++ myproject/first_App/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # return HttpResponse("<h1> Hello world </h1>")
-    return render(request, "home.html")
+    return render(request, "home.html", {'name' : 'Girish'})
 
