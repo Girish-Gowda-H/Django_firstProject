d83da7617883865bd99327b3b57effa9625647ee
Girish-Gowda <109037149+Girish-Gowda-H@users.noreply.github.com>
2024-04-16 17:40:16 +0530

    passing multiple objects and dynamically render the data
    the above code to copy static folder content to assets
    python manage.py collectstatic
    in model.py creates car class
    using that class created 3 objects and pass to the template

--- myproject/staticfiledemo/models.py
+++ myproject/staticfiledemo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class brands:
+    id: int
+    name: str
+    brand: str
+    price: int
+    img: str
--- myproject/staticfiledemo/views.py
+++ myproject/staticfiledemo/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import car
 
 # Create your views here.
 def index(request):
     # return HttpResponse("<h1> Hello world </h1>")
-    return render(request, "index.html")
+    # brands = [
+    #     {'id':1 ,'name' :'a' , 'img':'br1.png'},
+    #     {'id':2 ,'name' :'b' , 'img':'br2.png'},
+    #     {'id':3 ,'name' :'c' , 'img':'br3.png'},
+    #     {'id':4 ,'name' :'d' , 'img':'br4.png'},
+    # ]
+    car1 = car()
+    car1.id = 1
+    car1.name = 'a'
+    car1.img = 'br1.png'
+
+    car2 = car()
+    car2.id = 2
+    car2.name = 'b'
+    car2.img = 'br2.png'
+    
+    car3 = car()
+    car3.id = 3
+    car3.name = 'c'
+    car3.img = 'br3.png'
+    
+    cars = [car1,car2,car3]
+
+    return render(request, "index.html" , {'cars' : cars})
 
 
 
