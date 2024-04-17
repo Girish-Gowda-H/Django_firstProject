f9472b3e505cfa9fe55e5729eb2210d8ca7b8642
Girish-Gowda <109037149+Girish-Gowda-H@users.noreply.github.com>
2024-04-17 14:54:59 +0530

    migration

--- myproject/staticfiledemo/models.py
+++ myproject/staticfiledemo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class car:
-    id: int
-    name: str
-    brand: str
-    price: int
-    img: str
+class car(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField()
+    price = models.IntegerField()
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    offer = models.BooleanField(default=False)
