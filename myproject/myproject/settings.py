daa2b9bc2233ab6a2252af962fcafdb7bf95d757
Girish-Gowda <109037149+Girish-Gowda-H@users.noreply.github.com>
2024-04-17 18:51:21 +0530

    data added to table from django admin panel and also image upload to the database
    admin panel
    step1:create superuser >>python manage.py createsuperuser
    step2: in browser localhost:8000/admin (open the admin panel)
    step3: to add class to admin panel
           in app admin.py file register the class
           "admin.site.register(class_name)"

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'assets')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for image upload to database
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- myproject/staticfiledemo/views.py
+++ myproject/staticfiledemo/views.py
@@ -11,27 +11,23 @@ def index(request):
     #     {'id':3 ,'name' :'c' , 'img':'br3.png'},
     #     {'id':4 ,'name' :'d' , 'img':'br4.png'},
     # ]
-    car1 = car()
-    car1.id = 1
-    car1.name = 'a'
-    car1.img = 'br1.png'
+    # car1 = car()
+    # car1.id = 1
+    # car1.name = 'a'
+    # car1.img = 'br1.png'
 
-    car2 = car()
-    car2.id = 2
-    car2.name = 'b'
-    car2.img = 'br2.png'
+    # car2 = car()
+    # car2.id = 2
+    # car2.name = 'b'
+    # car2.img = 'br2.png'
     
-    car3 = car()
-    car3.id = 3
-    car3.name = 'c'
-    car3.img = 'br3.png'
-
-    car4 = car()
-    car3.id = 3
-    car3.name = 'c'
-    car3.img = 'br3.png'
+    # car3 = car()
+    # car3.id = 3
+    # car3.name = 'c'
+    # car3.img = 'br3.png'
     
-    cars = [car1,car2,car3]
+    # cars = [car1,car2,car3]
+    cars = car.objects.all()
 
     return render(request, "index.html" , {'cars' : cars})
 
